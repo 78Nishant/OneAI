b29fed6e9f65b15fa9ab1d7c45d372d5f2a3201c
Nishant Kumar <145116546+78Nishant@users.noreply.github.com>
2025-03-18 00:00:52 +0530

    Added Models Name file

---
+++ frontend/model.js
@@ -0,0 +1,20 @@
+//Name of the model and the provider here
+
+const models=[
+    {
+        model_name:"Qwen/QwQ-32B",
+        model_provider:"fireworks-ai",
+    },
+    {
+        model_name:"deepseek-ai/DeepSeek-R1",
+        model_provider:"nebius",
+    },
+    {
+        model_name:"EleutherAI/gpt-neo-2.7B",
+        model_provider:"openai",
+    },
+    {
+        model_name:"EleutherAI/gpt-neo-1.3B",
+        model_provider:"openai",
+    }
+]
